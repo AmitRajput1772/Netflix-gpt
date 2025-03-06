e417d1763011693458846045ff863263bda330cc
Amit rajput <amitrajput1772@gmail.com>
2025-03-06 12:38:35 +0530

    some changes in firebaseconfig and than redeployed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,16 +8,18 @@ import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDVYfof3GB1j5axDEEONpRyb9t2t2tFt6s",
-    authDomain: "netflixgpt-f8433.firebaseapp.com",
-    projectId: "netflixgpt-f8433",
-    storageBucket: "netflixgpt-f8433.firebasestorage.app",
-    messagingSenderId: "325946990168",
-    appId: "1:325946990168:web:201b2f1895e3d9e314b42d",
-    measurementId: "G-YJPJEQD6ML"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
-export const auth = getAuth();
+
+export { auth, analytics };
